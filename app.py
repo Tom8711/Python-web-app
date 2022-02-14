e7e2e6bf104ee92c3b2031490cfb0d26e6107d1b
Tom <tvanlente@gmail.com>
2022-02-14 14:17:23 +0100

    Eerste commit python webapp

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/tweede/<naam>")
+def hello_world2(naam):
+    return f"<p>Hello {naam}, tweede!</p>"
