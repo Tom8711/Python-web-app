14c4442988959b5bc5e0ce3a3841c037e163ec58
Tom <tvanlente@gmail.com>
2022-02-14 15:11:19 +0100

    methode toegevoegd

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ def hello_world():
 @app.route("/tweede/<naam>")
 def hello_world2(naam):
     return f"<p>Hello {naam}, tweede!</p>"
+
+@app.route("/derde/<nummer>")
+def hello_world3(nummer):
+    return f"<p>Hello dit nummer: {nummer} staat in de adresbalk! </p>"
